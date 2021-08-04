7026d18da38c3ab85b2e05d35a31de742d432350
paty-oliveira <patriciia.mota@gmail.com>
2021-08-04 18:16:29 +0100

    Implement Pangram challenge

--- challenge02/src/main/java/Pangram.java
+++ challenge02/src/main/java/Pangram.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
 /**
  * A pangram is a sentence that contains every single letter of the alphabet at least once.
  * For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because
@@ -8,4 +11,11 @@
  */
 
 public class Pangram {
+
+    public Boolean isPangram(String inputSentence) {
+        return inputSentence.chars()
+                .filter(letter -> letter >= 'a' && letter <= 'z')
+                .distinct()
+                .count() == 26;
+    }
 }
--- challenge02/src/test/java/PangramTest.java
+++ challenge02/src/test/java/PangramTest.java
@@ -1,5 +1,43 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class PangramTest {
+    
+    public Pangram pangramChecker;
+    
+    public PangramTest() {
+        this.pangramChecker = new Pangram();
+    }
 
+    @Test
+    void shouldReturnFalseWhenSentenceInputIsEmpty() throws Exception{
+        String inputSentence = "";
+        Boolean actualResult = this.pangramChecker.isPangram(inputSentence);
+
+        Assertions.assertFalse(actualResult);
+    }
+
+    @Test
+    void shouldReturnTrueWhenSentenceInputHasAllAlphabetLettersInAlphabeticOrder() throws Exception {
+        String inputSentence = "abcdefghijklmnopqrstuvwxyz";
+        Boolean actualResult = this.pangramChecker.isPangram(inputSentence);
+
+        Assertions.assertTrue(actualResult);
+    }
+
+    @Test
+    void shouldReturnFalseWhenSentenceInputHasNotAllAlphabetLetters() throws Exception{
+        String inputSentence = "aeiou";
+        Boolean actualResult = this.pangramChecker.isPangram(inputSentence);
+
+        Assertions.assertFalse(actualResult);
+    }
+
+    @Test
+    void shouldReturnTrueWhenSentenceInputHasEveryLetterOfAlphabet() throws Exception {
+        String inputSentence = "The quick brown fox jumps over the lazy dog";
+        Boolean actualResult = this.pangramChecker.isPangram(inputSentence);
+
+        Assertions.assertTrue(actualResult);
+    }
 }
