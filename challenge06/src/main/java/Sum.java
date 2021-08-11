26cc2cedb55a1aba479e457c305ce52e54496c61
paty-oliveira <patriciia.mota@gmail.com>
2021-08-11 16:48:33 +0100

    Implement Sum of Numbers challenge

--- challenge06/src/main/java/Sum.java
+++ challenge06/src/main/java/Sum.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * Given two integers a and b, which can be positive or negative, find the sum of all the integers between
@@ -6,4 +10,33 @@
  */
 
 public class Sum {
+
+    public int getSum(int number1, int number2) {
+        List<Integer> numbers = putNumberInList(number1, number2);
+        int maxNumber = getMaximumNumber(numbers);
+        int minNumber = getMinimumNumber(numbers);
+        int sum = 0;
+
+        for (int number = minNumber; number <= maxNumber; number++){
+            sum += number;
+        }
+
+        return sum;
+    }
+
+    public int getMinimumNumber(List<Integer> numbers) {
+        return Collections.min(numbers);
+    }
+
+    public int getMaximumNumber(List<Integer> numbers) {
+        return Collections.max(numbers);
+    }
+
+    public List<Integer> putNumberInList(int number1, int number2){
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(number1);
+        numbers.add(number2);
+
+        return numbers;
+    }
 }
--- challenge06/src/test/java/SumTest.java
+++ challenge06/src/test/java/SumTest.java
@@ -1,5 +1,62 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SumTest {
+    private final Sum sum;
 
+    public SumTest() {
+        this.sum = new Sum();
+    }
+
+    @Test
+    void shouldReturnSumOfTwoConsecutiveNumbers() throws Exception {
+        int inputNumber1 = 1;
+        int inputNumber2 = 0;
+        int actualResult = this.sum.getSum(inputNumber1, inputNumber2);
+        int expectedResult = 1;
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldReturnAccumulatedSumOfOnePositiveNumberAndNegativeNumbers() throws Exception {
+        int inputNumber1 = -1;
+        int inputNumber2 = 2;
+        int actualResult = this.sum.getSum(inputNumber1, inputNumber2);
+        int expectedResult = 2;
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldReturnAccumulatedSumOfTwoPositiveNumbers() throws Exception {
+        int inputNumber1 = 9;
+        int inputNumber2 = 20;
+        int actualResult = this.sum.getSum(inputNumber1, inputNumber2);
+        int expectedResult = 174;
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldReturnAccumulatedSumOfTwoNegativeNumbers() throws Exception {
+        int inputNumber1 = -4;
+        int inputNumber2 = -2;
+        int actualResult = this.sum.getSum(inputNumber1, inputNumber2);
+        int expectedResult = -9;
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldReturnTheSameNumberIfTheTwoNumbersAreEquals() throws Exception {
+        int inputNumber1 = 1;
+        int inputNumber2 = 1;
+        int actualResult = this.sum.getSum(inputNumber1, inputNumber2);
+        int expectedResult = 1;
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
 }
