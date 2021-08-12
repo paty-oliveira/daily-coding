389fb8f37d84636c982c96e96fd53a54c3f746c4
paty-oliveira <patriciia.mota@gmail.com>
2021-08-12 18:37:22 +0100

    Implement Lost Without a Map challenge

--- challenge07/src/main/java/Maps.java
+++ challenge07/src/main/java/Maps.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Given an array of integers, return a new array with each value doubled.
  * Example:
@@ -5,4 +8,10 @@
  */
 
 public class Maps {
+    public List<Integer> maps(List<Integer> inputList) {
+        return inputList
+                .stream()
+                .map(number -> number * 2)
+                .collect(Collectors.toList());
+    }
 }
--- challenge07/src/test/java/MapsTest.java
+++ challenge07/src/test/java/MapsTest.java
@@ -1,5 +1,64 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 
 class MapsTest {
+    private final Maps map;
 
+    public MapsTest() {
+        this.map = new Maps();
+    }
+
+    @Test
+    void shouldReturnAnEmptyListWhenInputListIsEmpty() throws Exception {
+        List<Integer> inputList = Collections.emptyList();
+        List<Integer> actualResult = this.map.maps(inputList);
+        List<Integer> expectedResult = Collections.emptyList();
+
+        Assertions.assertTrue(actualResult.containsAll(expectedResult)
+                && expectedResult.containsAll(actualResult));
+    }
+
+    @Test
+    void shouldReturnAListOfOnePositiveIntegerWithValueDoubled() throws Exception {
+        List<Integer> inputList = Collections.singletonList(1);
+        List<Integer> actualResult = this.map.maps(inputList);
+        List<Integer> expectedResult = Collections.singletonList(2);
+
+        Assertions.assertTrue(actualResult.containsAll(expectedResult)
+                && expectedResult.containsAll(actualResult));
+    }
+
+    @Test
+    void shouldReturnAListOfTwoPositiveIntegersWithValuesDoubled() throws Exception {
+        List<Integer> inputList = Arrays.asList(1, 2);
+        List<Integer> actualResult = this.map.maps(inputList);
+        List<Integer> expectedResult = Arrays.asList(2, 4);
+
+        Assertions.assertTrue(actualResult.containsAll(expectedResult)
+                && expectedResult.containsAll(actualResult));
+    }
+
+    @Test
+    void shouldReturnAListOfThreePositiveIntegersWithValuesDoubled() throws Exception {
+        List<Integer> inputList = Arrays.asList(1, 2, 3);
+        List<Integer> actualResult = this.map.maps(inputList);
+        List<Integer> expectedResult = Arrays.asList(2, 4, 6);
+
+        Assertions.assertTrue(actualResult.containsAll(expectedResult)
+                && expectedResult.containsAll(actualResult));
+    }
+
+    @Test
+    void shouldReturnAListOfThreeNegativeIntegersWithValuesDoubled() throws Exception {
+        List<Integer> inputList = Arrays.asList(-1, -2, -5);
+        List<Integer> actualResult = this.map.maps(inputList);
+        List<Integer> expectedResult = Arrays.asList(-2, -4, -10);
+
+        Assertions.assertTrue(actualResult.containsAll(expectedResult)
+                && expectedResult.containsAll(actualResult));
+    }
 }
