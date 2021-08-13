094ad2d8cc4e21048fee66cdc3ec317f7baa141d
paty-oliveira <patriciia.mota@gmail.com>
2021-08-13 14:40:35 +0100

    Implement Two To One challenge

--- challenge08/src/main/java/TwoToOne.java
+++ challenge08/src/main/java/TwoToOne.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 /**
  * Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible,
  * containing distinct letters - each taken only once - coming from s1 or s2.
@@ -11,5 +14,14 @@
  * longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
  */
 
-public class TwoToOne {
+class LettersConvertor {
+
+    public String distinctLetters(String inputLetters1, String inputLetters2) {
+        String distinctLetters = inputLetters1 + inputLetters2;
+
+        return Arrays.stream(distinctLetters.split(""))
+                .distinct()
+                .sorted()
+                .collect(Collectors.joining());
+    }
 }
--- challenge08/src/test/java/TwoToOneTest.java
+++ challenge08/src/test/java/TwoToOneTest.java
@@ -1,5 +1,61 @@
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TwoToOneTest {
 
+    private final LettersConvertor convertor;
+
+    public TwoToOneTest() {
+        this.convertor = new LettersConvertor();
+    }
+
+    @Test
+    void shouldReturnAnEmptyStringWhenInputStatementsAreEmpty() {
+        String inputLetters1 = "";
+        String inputLetters2 = "";
+        String actualResult = this.convertor.distinctLetters(inputLetters1, inputLetters2);
+        String expectedResult = "";
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldReturnTheInputStatementsWithoutDuplicatesLettersWhenOneOfInputStatementIsEmpty() {
+        String inputLetters1 = "abbbbccccfffffff";
+        String inputLetters2 = "";
+        String actualResult = this.convertor.distinctLetters(inputLetters1, inputLetters2);
+        String expectedResult = "abcf";
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldReturnTheResultOfConcatenationOfTwoInputStatementsWithoutDuplicatesLetters() {
+        String inputLetters1 = "abc";
+        String inputLetters2 = "def";
+        String actualResult = this.convertor.distinctLetters(inputLetters1, inputLetters2);
+        String expectedResult = "abcdef";
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldReturnTheResultOfConcatenationOfTwoInputStatementsWithDuplicatesLetters() {
+        String inputLetters1 = "abcdef";
+        String inputLetters2 = "efghij";
+        String actualResult = this.convertor.distinctLetters(inputLetters1, inputLetters2);
+        String expectedResult = "abcdefghij";
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldReturnTheResultOfConcatenationOfTwoInputStatementsWithDuplicatesLettersInSortedWay() {
+        String inputLetters1 = "xyaabbbccccdefww";
+        String inputLetters2 = "xxxxyyyyabklmopq";
+        String actualResult = this.convertor.distinctLetters(inputLetters1, inputLetters2);
+        String expectedResult = "abcdefklmopqwxy";
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
